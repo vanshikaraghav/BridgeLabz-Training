e1310d2b8200c699b7f2b8a32b840379c7ae686e
Vanshika Raghav <vanshikathakur46@gmail.com>
2025-12-18 16:54:09 +0530

    assignment day1

---
+++ core-java-practice/gcr-codebase/programming elements/SideOfSquare.java
@@ -0,0 +1,13 @@
+package programming_elements;
+import java.util.Scanner;
+public class SideOfSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc=new Scanner(System.in);
+     int p=sc.nextInt();
+     int s=p/4;
+     System.out.println("The length of the side is "+s+ " whose perimeter is "+p);
+	}
+
+}
