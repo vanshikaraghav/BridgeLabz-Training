d45036ab96a221f1becc5a7590e33bbfaf067c25
Vanshika Raghav <vanshikathakur46@gmail.com>
2026-02-06 10:57:49 +0530

    Collectors

---
+++ java8-features-practice/gcr-codebase/collectors/WordCounter.java
@@ -0,0 +1,17 @@
+package collectors;
+import java.util.*;
+import java.util.stream.Collectors;
+public class WordCounter {
+     public static void main(String[] args) {
+    	 String paragraph="java is fun and java is powerful";
+    	 Map<String,Integer>wordCount=Arrays.stream(paragraph.split(" "))
+    			 .collect(Collectors.toMap(
+    					 word->word,
+    			         word->1,
+    			         Integer::sum));
+    	 System.out.println(wordCount);
+    	 
+    
+     }
+     }
+
