26fbf557b5a210fd136ac2445f752a6238ae2953
Vanshika Raghav <vanshikathakur46@gmail.com>
2026-02-08 23:19:31 +0530

    Designing Practice

---
+++ designing-practice/gcr-codebase/smart_university_library/Book.java
@@ -0,0 +1,45 @@
+package smart_university_library;
+
+public class Book {
+	private String title;
+	private String author;
+	private String edition;
+	private String genre;
+
+	private Book(BookBuilder builder) {
+		this.title = builder.title;
+		this.author = builder.author;
+		this.edition = builder.edition;
+		this.genre = builder.genre;
+
+	}
+	public String getTiltle() {
+		return title;
+	}
+	
+	public static class BookBuilder{
+		private String title;
+		private String author;
+		private String edition;
+		private String genre;
+
+		public BookBuilder(String title) { 
+			this.title = title;
+		}
+		public BookBuilder author(String author) {
+			this.author = author;
+			return this;
+		}
+		public BookBuilder edition(String edition) {
+			this.edition = edition;
+			return this;
+		}
+		public BookBuilder genre(String genre) { 
+			this.genre = genre; 
+			return this;
+		}
+		public Book build() {
+			return new Book(this);
+		}
+	}
+}
